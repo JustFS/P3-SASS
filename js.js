a1ef6497b34e54609a1ac17bafb91bf945b4a29c
Julien Az <julien.azbrg@gmail.com>
2020-01-10 19:14:16 +0100

    version finale

--- js.js
+++ js.js
@@ -1,15 +1,8 @@
- const card = $(".perspective__card");
+const card = $(".perspective__3d");
 
- $("body").on("mousemove", function (t) {
-     let vertical = -($(window).innerWidth() / 2 - t.pageX) / 60,
-         horizontal = ($(window).innerHeight() / 2 - t.pageY) / 60;
-     card.attr("style", "transform: rotateY(" + vertical + "deg) rotateX(" + horizontal + "deg)")
- })
- 
- 
-
-
-
-
-
-
+$("body").on("mousemove", function (t) {
+    let vertical = -($(window).innerWidth() / 2 - t.pageX) / 70,
+        horizontal = ($(window).innerHeight() / 2 - t.pageY) / 70;
+    card.attr("style", "transform: rotateY(" + vertical + "deg) rotateX(" + horizontal + "deg)")
+});
+  
